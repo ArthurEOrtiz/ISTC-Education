cb04cfa4abb169ca7bc9dc1d43e9e045913d72b0
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-09-04 07:43:34 -0600

    Testing

--- istc-education-spa/app/page.tsx
+++ istc-education-spa/app/page.tsx
@@ -1,4 +1,5 @@
-const Home:React.FC = () => {
+const Home:React.FC = async () => {
+
   return (
     <div className="w-full flex justify-center items-center">
       <p>Welcome to the ISTC Education website</p>
