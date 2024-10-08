3575f58dd7489c4e90484fa3e0c3464572935c05
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-10-08 10:46:06 -0600

    update ui

--- istc-education-spa/app/admin/page.tsx
+++ istc-education-spa/app/admin/page.tsx
@@ -2,25 +2,25 @@ import Link from "next/link"
 
 const AdminPage: React.FC = async () => {
     return (
-        <div className="w-full flex flex-col items-center space-y-2">
-            <h1 className="text-3xl font-bold"> Admin Dashboard </h1>
-            <div className="border border-info rounded-md space-y-2 p-4">
-                <h2 className="text-xl font-bold">Admin Actions</h2>
+        <div className="w-full max-w-xl flex flex-col mx-auto space-y-2">
+            <h1 className="text-3xl font-bold text-center"> Admin Dashboard </h1>
+            <div className="w-full border border-info rounded-md space-y-2 p-4">
+                <h2 className="text-xl font-bold">Edit</h2>
                 <div className="flex gap-2">
                     <Link href="/course/edit" className="btn btn-primary">
-                        Edit Courses
+                        Courses
                     </Link>
                     <Link href="/user" className="btn btn-primary">
-                        Edit Users
+                        Users
                     </Link>
                     <Link href="/topic/edit" className="btn btn-primary">
-                        Edit Topics
+                        Topics
                     </Link>
                 </div>
             </div>
 
-            <div className="border border-info rounded-md space-y-2 p-4">
-                <h2 className="text-xl font-bold">Admin Reports</h2>
+            <div className="w-full border border-info rounded-md space-y-2 p-4">
+                <h2 className="text-xl font-bold">Reports</h2>
                 <div className="flex gap-2">
                     <Link href="/report/course" className="btn btn-primary">
                         Course Reports
